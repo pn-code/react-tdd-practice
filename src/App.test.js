ad7c5d22b177b5b95f2715973213236cdb00f6b4
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-31 20:42:26 -0800

    Adding Test for DOM

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,32 @@
-import { test, expect } from "vitest";
-
-test("1 + 1", () => {
-  expect(1 + 1).toEqual(2);
-});
+import { render, renderHook, screen } from "@testing-library/react";
+import App from "./App";
 
 function sum(...nums) {
   return nums.reduce((a, b) => a + b, 0);
 }
 
+test("1 + 1", () => {
+  expect(1 + 1).toEqual(2);
+});
+
 test("1 + 2 + 3", () => {
   expect(sum(1, 2, 3)).toEqual(6);
+});
+
+test("10 numbers", () => {
+  expect(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)).toEqual(55);
+});
+
+test("0 numbers", () => {
+  expect(sum()).toEqual(0);
+});
+
+test("1 number", () => {
+  expect(sum(1)).toEqual(1);
+});
+
+test("renders header", () => {
+  render(App())
+  const message = screen.queryByText(/Hello World/i);
+  expect(message).toBeInTheDocument();
 });
