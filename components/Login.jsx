5ed4783c2d9fddf3a266abf18560cf07baa03412
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-31 21:29:43 -0800

    Added span that features a testid of "error"

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div className='container'>
-        <form>
-            <input placeholder="username" type="text"/>
-            <input placeholder="password" type="password"/>
-            <button>Login</button>
-        </form>
+    <div className="container">
+      <form>
+        <input placeholder="username" type="text" />
+        <input placeholder="password" type="password" />
+        <button disabled={true}>Login</button>
+        <span data-testid="error">Something went wrong!</span>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
