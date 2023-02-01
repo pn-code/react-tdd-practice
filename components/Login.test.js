3ce8473dcc05f60ad700eebf4f757d4e78e15b4d
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-31 21:26:22 -0800

    Added describe block to check if inputs are empty

--- components/Login.test.js
+++ components/Login.test.js
@@ -1,20 +1,36 @@
 import { render, screen } from "@testing-library/react";
 import Login from "./Login";
 
-test("username input should be rendered", () => {
-  render(Login());
-  const userInputElement = screen.getByPlaceholderText(/username/i);
-  expect(userInputElement).toBeInTheDocument();
+describe("render form elements", () => {
+  test("username input should be rendered", () => {
+    render(Login());
+    const userInputElement = screen.getByPlaceholderText(/username/i);
+    expect(userInputElement).toBeInTheDocument();
+  });
+
+  test("password input should be rendered", () => {
+    render(Login());
+    const passwordInputElement = screen.getByPlaceholderText(/password/i);
+    expect(passwordInputElement).toBeInTheDocument();
+  });
+
+  test("login button should be rendered", () => {
+    render(Login());
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+  });
 });
 
-test("password input should be rendered", () => {
-  render(Login());
-  const passwordInputElement = screen.getByPlaceholderText(/password/i);
-  expect(passwordInputElement).toBeInTheDocument();
-});
+describe("on start, inputs should be empty", () => {
+  test("username input should be rendered", () => {
+    render(Login());
+    const userInputElement = screen.getByPlaceholderText(/username/i);
+    expect(userInputElement.value).toBe("")
+  });
 
-test("login button should be rendered", () => {
-  render(Login());
-  const button = screen.getByRole("button");
-  expect(button).toBeInTheDocument();
+  test("password input should be rendered", () => {
+    render(Login());
+    const passwordInputElement = screen.getByPlaceholderText(/password/i);
+    expect(passwordInputElement.value).toBe("")
+  });
 });
