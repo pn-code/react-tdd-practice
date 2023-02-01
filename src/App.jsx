424a2ed730763359aeb84466d106a870a0591d54
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-31 21:12:46 -0800

    Commenting out previous tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
+import Login from "../components/Login";
 import "./App.css";
 
 function App() {
-  const a = 2;
-  const b = 4;
+  // const a = 2;
+  // const b = 4;
 
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <ul>
-        <li>Orange</li>
-        <li>Strawberry</li>
-        <li>Kiwi</li>
-      </ul>
-      <h2 data-testid="mytestid">Third DOM Test</h2>
-      <span title="sum">{a+b}</span>
+      {/* <div>
+        <h1>Hello World</h1>
+        <ul>
+          <li>Orange</li>
+          <li>Strawberry</li>
+          <li>Kiwi</li>
+        </ul>
+        <h2 data-testid="mytestid">Third DOM Test</h2>
+        <span title="sum">{a + b}</span>
+      </div> */}
+
+      <Login />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,52 +1,52 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-// VITEST TESTS
-function sum(...nums) {
-  return nums.reduce((a, b) => a + b, 0);
-}
+// // VITEST TESTS
+// function sum(...nums) {
+//   return nums.reduce((a, b) => a + b, 0);
+// }
 
-test("1 + 1", () => {
-  expect(1 + 1).toEqual(2);
-});
+// test("1 + 1", () => {
+//   expect(1 + 1).toEqual(2);
+// });
 
-test("1 + 2 + 3", () => {
-  expect(sum(1, 2, 3)).toEqual(6);
-});
+// test("1 + 2 + 3", () => {
+//   expect(sum(1, 2, 3)).toEqual(6);
+// });
 
-test("10 numbers", () => {
-  expect(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)).toEqual(55);
-});
+// test("10 numbers", () => {
+//   expect(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)).toEqual(55);
+// });
 
-test("0 numbers", () => {
-  expect(sum()).toEqual(0);
-});
+// test("0 numbers", () => {
+//   expect(sum()).toEqual(0);
+// });
 
-test("1 number", () => {
-  expect(sum(1)).toEqual(1);
-});
+// test("1 number", () => {
+//   expect(sum(1)).toEqual(1);
+// });
 
-// DOM TESTS
-test("renders header", () => {
-  render(App());
-  const message = screen.queryByText(/Hello World/i);
-  expect(message).toBeInTheDocument();
-});
+// // DOM TESTS
+// test("renders header", () => {
+//   render(App());
+//   const message = screen.queryByText(/Hello World/i);
+//   expect(message).toBeInTheDocument();
+// });
 
-test("checks for 3 list items", () => {
-  render(App());
-  const listItems = screen.getAllByRole("listitem");
-  expect(listItems).toHaveLength(3);
-});
+// test("checks for 3 list items", () => {
+//   render(App());
+//   const listItems = screen.getAllByRole("listitem");
+//   expect(listItems).toHaveLength(3);
+// });
 
-test("checks for header with data test id", () => {
-  render(App());
-  const data = screen.getByTestId("mytestid");
-  expect(data).toBeInTheDocument();
-});
+// test("checks for header with data test id", () => {
+//   render(App());
+//   const data = screen.getByTestId("mytestid");
+//   expect(data).toBeInTheDocument();
+// });
 
-test("sum", () => {
-  render(App());
-  const sum = screen.getByTitle("sum");
-  expect(sum.textContent).toEqual(6);
-});
+// test("sum", () => {
+//   render(App());
+//   const sum = screen.getByTitle("sum");
+//   expect(sum.textContent).toBe("6");
+// });
